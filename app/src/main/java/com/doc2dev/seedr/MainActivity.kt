b6a271b6b1acca862b7689b489a5d5259dc92848
Eston Karumbi <eston.karumbi@andela.com>
2018-12-04 20:29:13 +0300

    Migrate project dependencies to AndroidX

--- app/src/main/java/com/doc2dev/seedr/MainActivity.kt
+++ app/src/main/java/com/doc2dev/seedr/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.doc2dev.seedr
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
